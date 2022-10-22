f1f2a44dd6aa1af1ddb1a25dff7b45e967a43463
hasan <hasanakoz1986@gmail.com>
2022-10-22 13:54:59 +0300

    useparams, uselocation, useNavigate done

--- src/pages/PersonDetail.jsx
+++ src/pages/PersonDetail.jsx
@@ -1,8 +1,27 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
+
 const PersonDetail = () => {
   const { id } = useParams();
   console.log(id);
-  return <div>PersonDetail</div>;
+
+  const navigate = useNavigate();
+
+  const { state } = useLocation();
+  console.log(state);
+  return (
+    // <div>person</div>
+    <div className="container text-center">
+      <h3>
+        {state.first_name} {state.last_name}
+      </h3>
+      <img className="rounded" src={state.avatar} alt="" />
+      <p>{state.email}</p>
+      <div>
+        <button onClick={() => navigate("/")}>Go Home</button>
+        <button onClick={() => navigate(-1)}>Go Back</button>
+      </div>
+    </div>
+  );
 };
 
 export default PersonDetail;
